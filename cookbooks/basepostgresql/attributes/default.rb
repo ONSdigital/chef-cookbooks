146413d4832cd6112e4fcdc6e26b1140fd4b70fd
John Topley <john.topley@ons.gov.uk>
2015-08-25 11:46:00 +0100

    Tidied up formatting

--- cookbooks/basepostgresql/attributes/default.rb
+++ cookbooks/basepostgresql/attributes/default.rb
@@ -6,30 +6,30 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-default['basepostgresql']['postgres_user']           = 'postgres'
+default['basepostgresql']['postgres_user']          = 'postgres'
 
-default['postgresql']['password']['postgres']        = 'postgres'
-default['postgresql']['enable_pgdg_yum']             = true
-default['postgresql']['database_name']               = 'postgres'
-default['postgresql']['version']                     = '9.3'
-default['postgresql']['dir']                         = '/var/lib/pgsql/9.3/data'
-default['postgresql']['client']['packages']          = ['postgresql93', 'postgresql93-devel']
-default['postgresql']['contrib']['packages']         = ['postgresql93-contrib']
-default['postgresql']['server']['packages']          = ['postgresql93-server']
+default['postgresql']['password']['postgres']       = 'postgres'
+default['postgresql']['enable_pgdg_yum']            = true
+default['postgresql']['database_name']              = 'postgres'
+default['postgresql']['version']                    = '9.3'
+default['postgresql']['dir']                        = '/var/lib/pgsql/9.3/data'
+default['postgresql']['client']['packages']         = ['postgresql93', 'postgresql93-devel']
+default['postgresql']['contrib']['packages']        = ['postgresql93-contrib']
+default['postgresql']['server']['packages']         = ['postgresql93-server']
 
 if platform_family?('rhel') and node['platform_version'].to_i >= 7
-  default['postgresql']['server']['service_name']    = 'postgresql-9.3.service'
+  default['postgresql']['server']['service_name']   = 'postgresql-9.3.service'
 else
-  default['postgresql']['server']['service_name']    = 'postgresql93'
+  default['postgresql']['server']['service_name']   = 'postgresql93'
 end
 
-default['postgresql']['config']['data_directory']    = '/var/lib/pgsql/9.3/data'
-default['postgresql']['config']['listen_addresses']  = '*'
-default['postgresql']['config']['log_timezone']      = 'GB'
-default['postgresql']['config']['lc_messages']       = 'en_GB.UTF-8'
-default['postgresql']['config']['lc_monetary']       = 'en_GB.UTF-8'
-default['postgresql']['config']['lc_numeric']        = 'en_GB.UTF-8'
-default['postgresql']['config']['lc_time']           = 'en_GB.UTF-8'
+default['postgresql']['config']['data_directory']   = '/var/lib/pgsql/9.3/data'
+default['postgresql']['config']['listen_addresses'] = '*'
+default['postgresql']['config']['log_timezone']     = 'GB'
+default['postgresql']['config']['lc_messages']      = 'en_GB.UTF-8'
+default['postgresql']['config']['lc_monetary']      = 'en_GB.UTF-8'
+default['postgresql']['config']['lc_numeric']       = 'en_GB.UTF-8'
+default['postgresql']['config']['lc_time']          = 'en_GB.UTF-8'
 
 default['postgresql']['contrib']['extensions'] = [
   'dblink',
