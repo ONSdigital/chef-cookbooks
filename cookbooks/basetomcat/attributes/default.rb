4e328268c5495f4048d8e680478aac619d639e1f
John Topley <john.topley@ons.gov.uk>
2015-09-18 13:37:48 +0100

    Added attribute for path to the Tomcat webapps directory

--- cookbooks/basetomcat/attributes/default.rb
+++ cookbooks/basetomcat/attributes/default.rb
@@ -7,6 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 default['basetomcat']['connector_port'] = '8180'
+default['basetomcat']['webapps_dir']    = '/usr/share/tomcat/webapps'
 
 default['basetomcat']['packages'] = {
   'rhel' => ['epel-release', 'tomcat', 'tomcat-admin-webapps']
