4bfa683a9f1baf00e20b2b3db494fa65f839a3d7
John Topley <john.topley@ons.gov.uk>
2015-10-30 10:40:06 +0000

    Creating a symbolic link from /home/fuse/.m2 to /opt/fuse/m2

--- cookbooks/basefuse/recipes/create_maven_repository.rb
+++ cookbooks/basefuse/recipes/create_maven_repository.rb
@@ -10,8 +10,10 @@ DATA_BAG_NAME        = node['baseos']['credentials_data_bag_name']
 
 MAVEN_REPOSITORY_DIR = node['basefuse']['maven_repository_dir']
 FUSE_USER            = node['basefuse']['fuse_user']
+FUSE_USER_HOME       = node['basefuse']['fuse_user_home']
 
 MAVEN_SETTINGS_FILE  = "#{MAVEN_REPOSITORY_DIR}/settings.xml"
+SYMBOLIC_LINK        = "#{FUSE_USER_HOME}/.m2"
 
 fuse_admin_user     = data_bag_item(DATA_BAG_NAME, 'fuse')['user']
 fuse_admin_password = data_bag_item(DATA_BAG_NAME, 'fuse')['password']
@@ -34,4 +36,18 @@ template MAVEN_SETTINGS_FILE do
   )
   action :create
   not_if "grep 'Chef' #{MAVEN_SETTINGS_FILE}"
+end
+
+if node.chef_environment == 'development'
+
+  # By default Fuse looks for a Maven repository in ~/.m2. We want it to look in
+  # the Maven repository we create instead. Creating a symbolic link makes
+  # commands such as fabric:watch work properly with our setup.
+  link SYMBOLIC_LINK do
+    owner FUSE_USER
+    group FUSE_USER
+    to    MAVEN_REPOSITORY_DIR
+    action :create
+    not_if { ::File.exist?(SYMBOLIC_LINK) }
+  end
 end
