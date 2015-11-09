0553f8861cab4f86613fbc643e1b463aca17e26a
John Topley <john.topley@ons.gov.uk>
2015-11-09 13:51:34 +0000

    Updated to latest version of Phusion Passenger

--- cookbooks/basepassenger/attributes/default.rb
+++ cookbooks/basepassenger/attributes/default.rb
@@ -6,7 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-default['basepassenger']['passenger_version'] = '5.0.15'
+default['basepassenger']['passenger_version'] = '5.0.21'
 default['basepassenger']['packages'] = {
   'rhel' => ['apr-devel', 'apr-util-devel', 'httpd-devel', 'libcurl-devel']
 }
