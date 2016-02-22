5c2dd1d24fafb91d58028c35c4d5a85d44aba7c2
John Topley <john.topley@ons.gov.uk>
2016-02-22 14:19:06 +0000

    Tidied up formatting

--- cookbooks/baseservicemix/attributes/default.rb
+++ cookbooks/baseservicemix/attributes/default.rb
@@ -6,12 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-default['baseservicemix']['servicemix_archive']            = 'apache-servicemix-6.1.0.zip'
-default['baseservicemix']['servicemix_archive_dir']        = 'chef/distros/apacheservicemix'
-default['baseservicemix']['servicemix_install_root']       = '/opt/servicemix'
-default['baseservicemix']['servicemix_install_dir']        = '/opt/servicemix/apache-servicemix-6.1.0'
-default['baseservicemix']['maven_repository_dir']          = '/opt/servicemix/m2'
-default['baseservicemix']['servicemix_user']               = 'servicemix'
-default['baseservicemix']['servicemix_user_home']          = '/home/servicemix'
-default['baseservicemix']['command_script']                = 'chef/scripts/apacheservicemix/base-properties'
-default['baseservicemix']['openwire_port']                 = '53445'
+default['baseservicemix']['servicemix_archive']      = 'apache-servicemix-6.1.0.zip'
+default['baseservicemix']['servicemix_archive_dir']  = 'chef/distros/apacheservicemix'
+default['baseservicemix']['servicemix_install_root'] = '/opt/servicemix'
+default['baseservicemix']['servicemix_install_dir']  = '/opt/servicemix/apache-servicemix-6.1.0'
+default['baseservicemix']['maven_repository_dir']    = '/opt/servicemix/m2'
+default['baseservicemix']['servicemix_user']         = 'servicemix'
+default['baseservicemix']['servicemix_user_home']    = '/home/servicemix'
+default['baseservicemix']['command_script']          = 'chef/scripts/apacheservicemix/base-properties'
+default['baseservicemix']['openwire_port']           = '53445'
