9a7f004cf14b6b7607dba26469f5ee2ed8768676
John Topley <john.topley@ons.gov.uk>
2016-03-11 09:49:55 +0000

    Added attribute guard to make the installation ot the web consoles idempotent

--- cookbooks/baseservicemix/recipes/install_web_console_features.rb
+++ cookbooks/baseservicemix/recipes/install_web_console_features.rb
@@ -8,6 +8,7 @@
 #
 SERVICEMIX_INSTALL_DIR = node['baseservicemix']['servicemix_install_dir']
 SERVICEMIX_USER        = node['baseservicemix']['servicemix_user']
+WEBCONSOLES_INSTALLED  = node['baseservicemix']['webconsoles_installed']
 
 SERVICEMIX_BIN_DIR     = "#{SERVICEMIX_INSTALL_DIR}/bin"
 JAVA_HOME              = '/usr/lib/jvm/jre'
@@ -19,4 +20,7 @@ execute command do
   environment 'JAVA_HOME' => JAVA_HOME
   command command
   action :run
+  only_if { WEBCONSOLES_INSTALLED == 'false' }
 end
+
+node.set['baseservicemix']['webconsoles_installed'] = 'true'
